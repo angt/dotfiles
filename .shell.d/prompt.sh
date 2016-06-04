7d067037ac35ce4175d6aafe5bd04eab1e0c3879
Adrien Gallouët <adrien@gallouet.fr>
2016-06-04 10:28:12 +0200

    Update prompt

--- .shell.d/prompt.sh
+++ .shell.d/prompt.sh
@@ -1,69 +1,35 @@
 # Heavily inspired by @necolas’s prompt: https://github.com/necolas/dotfiles
 
 prompt_git() {
-    local branchName="$(git symbolic-ref --quiet --short HEAD 2> /dev/null || \
-                        git rev-parse --short HEAD 2> /dev/null || \
-                        echo '(none)')";
-
-    echo "${1}${branchName}";
+    git symbolic-ref --quiet --short HEAD 2> /dev/null ||
+    git rev-parse --short HEAD 2> /dev/null ||
+    echo '(none)'
 }
 
-if tput setaf 1 &> /dev/null; then
-    tput sgr0; # reset colors
-    bold=$(tput bold);
-    reset=$(tput sgr0);
-#  Solarized colors, taken from http://git.io/solarized-colors.
-    black=$(tput setaf 0);
-    blue=$(tput setaf 33);
-    cyan=$(tput setaf 37);
-    green=$(tput setaf 64);
-    orange=$(tput setaf 166);
-    purple=$(tput setaf 125);
-    red=$(tput setaf 124);
-    violet=$(tput setaf 61);
-    white=$(tput setaf 15);
-    yellow=$(tput setaf 136);
-else
-    bold='';
-    reset="\e[0m";
-    black="\e[1;30m";
-    blue="\e[1;34m";
-    cyan="\e[1;36m";
-    green="\e[1;32m";
-    orange="\e[1;33m";
-    purple="\e[1;35m";
-    red="\e[1;31m";
-    violet="\e[1;35m";
-    white="\e[1;37m";
-    yellow="\e[1;33m";
-fi;
+bold="\[\e[1m\]"
+reset="\[\e[0m\]"
+black="\[\e[30m\]"
+red="\[\e[31m\]"
+green="\[\e[32m\]"
+yellow="\[\e[33m\]"
+blue="\[\e[34m\]"
+magenta="\[\e[35m\]"
+cyan="\[\e[36m\]"
+white="\[\e[97m\]"
 
-# Highlight the user name when logged in as root.
-if [[ "${USER}" == "root" ]]; then
-    userStyle="${red}";
-else
-    userStyle="${orange}";
-fi;
+userStyle="${magenta}"
+hostStyle="${blue}"
 
-# Highlight the hostname when connected via SSH.
-if [[ "${SSH_TTY}" ]]; then
-    hostStyle="${bold}${red}";
-else
-    hostStyle="${yellow}";
-fi;
+[ "${USER}" == "root" ] && userStyle="${red}"
+[ -n "${SSH_TTY}" ] && hostStyle="${red}"
 
-# Set the terminal title to the current working directory.
-PS1="\[\033]0;\w\007\]";
-PS1+="\[${bold}\]\n"; # newline
-PS1+="\[${userStyle}\]\u"; # username
-PS1+="\[${white}\] at ";
-PS1+="\[${hostStyle}\]\h"; # host
-PS1+="\[${white}\] in ";
-PS1+="\[${green}\]\w"; # working directory
-PS1+="\$(prompt_git \"${white} on ${violet}\")"; # Git repository details
-PS1+="\n";
-PS1+="\[${white}\]\$ \[${reset}\]"; # `$` (and reset color)
-export PS1;
+PS1="
+${bold}${userStyle}\u\
+${white} at ${hostStyle}\h\
+${white} in ${green}\w\
+${white} on ${yellow}\$(prompt_git)
+${white}\$ ${reset}"
+export PS1
 
-PS2="\[${yellow}\]→ \[${reset}\]";
-export PS2;
+PS2="${white}> ${reset}"
+export PS2
