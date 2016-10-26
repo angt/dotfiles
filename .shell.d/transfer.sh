4b2cc6fd56f456f17ecbf21038fef03e0cb04ae7
Adrien Gallouët <adrien@gallouet.fr>
2016-10-26 21:06:57 +0200

    Add transfer.sh

---
+++ .shell.d/transfer.sh
@@ -0,0 +1,8 @@
+tf() {
+    [ -f "$1" ] && curl -T "$1" https://transfer.sh/"$1"
+}
+
+tfz() {
+    # dont tar | curl
+    [ -e "$1" ] && tar czf "$1".tgz "$1" && curl -T "$1".tar https://transfer.sh/"$1".tgz
+}
