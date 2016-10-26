7d042e59d93ba5d355794cf8b506ff23dd792516
Adrien Gallouët <adrien@gallouet.fr>
2016-10-26 19:55:05 +0200

    Dont forget Darwin

--- .shell.d/go.sh
+++ .shell.d/go.sh
@@ -1,11 +1,15 @@
-DIR=go1.7.3
+VERSION=1.7.3
+OS=`uname | tr [:upper:] [:lower:]`
+ARCH=amd64
+DIR=go${VERSION}
+TGZ=${DIR}.${OS}-${ARCH}.tar.gz
 
 export GOROOT=~/.${DIR}
 export GOPATH=${GOPATH:-~/.go}
 
-shell_add_path "$GOROOT/bin"
-shell_add_path "$GOPATH/bin"
+shell_add_path "${GOROOT}/bin"
+shell_add_path "${GOPATH}/bin"
 
 go_install() {
-    curl https://storage.googleapis.com/golang/${DIR}.linux-amd64.tar.gz | tar xz -C ${GOROOT} --strip-components=1
+    curl https://storage.googleapis.com/golang/${TGZ} | tar xz -C ${GOROOT} --strip-components=1
 }
