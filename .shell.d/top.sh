177c0b5f07c2d420a051a0c5e1fc4c9a6c2b27b8
Adrien Gallouët <adrien@gallouet.fr>
2016-12-21 13:43:12 +0000

    Remove top.sh

--- .shell.d/top.sh
+++
@@ -1,3 +0,0 @@
-top() {
-    watch -tn 1 uptime \; ps -exk -%cpu -o pid,ppid,user,vsz,rss,%cpu,%mem,stat,args \| awk \''{ if ($9!="watch") print $1,$2,$3,$4,$5,$6,$7,$8,$9 }'\' \| column -t \| cut -b-\$\(tput cols\)
-}
