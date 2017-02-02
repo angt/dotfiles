caefe2d0253be108138fa12b270c989d9b6425c8
Adrien Gallouët <adrien@gallouet.fr>
2017-02-02 17:19:33 +0100

    Add ssh-agent

--- .shell.d/ssh.sh
+++ .shell.d/ssh.sh
@@ -1,1 +1,7 @@
+mkdir -p .ssh
+
 grep 'PRIVATE KEY' -lZR .ssh | xargs -0 chmod 400
+
+pgrep -u "$USER" ssh-agent > /dev/null || ssh-agent > ~/.ssh-agent-eval
+
+[ -z "$SSH_AGENT_PID" ] && eval "$(<~/.ssh-agent-eval)"
