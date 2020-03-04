b7ba62f14df8c4e47917ad032409c59ed1a7f9da
Adrien Gallouët <adrien@gallouet.fr>
2020-03-04 11:13:40 +0000

    Update
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/go.sh
+++ .shell.d/go.sh
@@ -1,15 +1,15 @@
-VERSION=1.11.1
-OS=$(uname | tr [:upper:] [:lower:])
+VERSION=1.14
+OS=$(uname | tr '[:upper:]' '[:lower:]')
 ARCH=amd64
-DIR=go${VERSION}
-TGZ=${DIR}.${OS}-${ARCH}.tar.gz
+DIR=go$VERSION
+TGZ=$DIR.$OS-$ARCH.tar.gz
 
-export GOROOT=~/.${DIR}
-export GOPATH=${GOPATH:-~/.go}
+export GOROOT="$HOME/.$DIR"
+export GOPATH="${GOPATH:-$HOME/.go}"
 
-shell_add_path "${GOROOT}/bin"
-shell_add_path "${GOPATH}/bin"
+shell_add_path "$GOROOT/bin"
+shell_add_path "$GOPATH/bin"
 
 shell_install_go() {
-    curl -sSf https://dl.google.com/go/${TGZ} | tar xz -C ${GOROOT} --strip-components=1
+    curl -sSf "https://dl.google.com/go/$TGZ" | tar xz -C "$GOROOT" --strip-components=1
 }
