f56b4b0b23568de3bc2be5a71eb4ffc7735cd166
Adrien Gallouët <adrien@gallouet.fr>
2020-04-23 20:23:50 +0000

    Allow xcopy/xpaste on macos
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/xclip.sh
+++ .shell.d/xclip.sh
@@ -1,2 +1,7 @@
-alias xcopy='xclip -selection clipboard -i'
-alias xpaste='xclip -selection clipboard -o'
+if command -v xclip >/dev/null; then
+	alias xcopy='xclip -selection clipboard -i'
+	alias xpaste='xclip -selection clipboard -o'
+elif command -v pbcopy >/dev/null; then
+	alias xcopy='pbcopy'
+	alias xpaste='pbpaste'
+fi
