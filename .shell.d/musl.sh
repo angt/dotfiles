fea37c35b664b2c1b27e0341a3453d290fbc7581
Adrien Gallouët <adrien@gallouet.fr>
2020-04-24 09:49:33 +0000

    Build in ~/.tmp
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/musl.sh
+++ .shell.d/musl.sh
@@ -1,4 +1,6 @@
 shell_install_musl() {
+	mkdir -p ~/.tmp
+	cd ~/.tmp || return
 	rm -rf musl-cross-make
 	git clone https://github.com/richfelker/musl-cross-make
 	cd musl-cross-make || return
--- .shell.d/secret.sh
+++ .shell.d/secret.sh
@@ -1,6 +1,7 @@
 shell_install_secret() {
 	mkdir -p ~/.tmp
 	cd ~/.tmp || return
+	rm -rf secret
 	git clone https://github.com/angt/secret --recursive
 	cd secret || return
 	make prefix=~/.local install
