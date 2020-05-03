ec4852fe29ed877add96cce9e02cce481729dbbb
Adrien Gallouët <adrien@gallouet.fr>
2020-05-03 21:58:29 +0000

    Cleanup ls
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/ls.sh
+++ .shell.d/ls.sh
@@ -1,16 +1,8 @@
 unset LS_COLORS
 export CLICOLOR=1
 
-ls --color=auto &>/dev/null && \
-	alias ls='ls --color=auto'
-
-alias l='ls --group-directories-first'
+ls --version 2>&1 | grep GNU >/dev/null && alias ls='ls --color=auto'
 alias l1='ls -1'
-alias lS='ls -LrsS'
 alias ll='ls -l'
-alias lA='ls -A'
 alias la='ls -a'
-alias lr='ls -R'
-alias llS='ll -rS'
-alias lla='la -l'
 alias lsd='la -d */'
