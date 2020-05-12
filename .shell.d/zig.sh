7cbe3c907a64c44bd381ea88f861856eb9fffdd7
Adrien Gallouët <adrien@gallouet.fr>
2020-05-12 14:55:29 +0000

    Add zig.sh
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

---
+++ .shell.d/zig.sh
@@ -0,0 +1,15 @@
+shell_install_zig() (
+	VERSION=0.6.0
+	OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+	ARCH=$(uname -m)
+	[ "$OS"  = darwin ] && OS=macos
+	[ "$ARCH" = amd64 ] && ARCH=x86_64
+	NAME=zig-$OS-$ARCH-$VERSION
+	mkdir -p ~/.zig ~/.local/bin ~/.local/lib
+	rm -rf ~/.zig/$NAME
+	rm -rf ~/.local/bin/zig
+	rm -rf ~/.local/lib/zig
+	curl -sSf https://ziglang.org/download/$VERSION/$NAME.tar.xz | tar -Jxf- -C ~/.zig
+	ln -sf ~/.zig/$NAME/zig     ~/.local/bin/zig
+	ln -sf ~/.zig/$NAME/lib/zig ~/.local/lib/zig
+)
