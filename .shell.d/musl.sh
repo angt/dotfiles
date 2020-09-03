ab9a310a4e0f3795af8583751c63de1023eb9db0
Adrien Gallouët <adrien@gallouet.fr>
2020-09-03 15:10:07 +0200

    Support native macos
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/musl.sh
+++ .shell.d/musl.sh
@@ -4,6 +4,9 @@ shell_install_musl() {
 	rm -rf musl-cross-make
 	git clone https://github.com/richfelker/musl-cross-make
 	cd musl-cross-make || return
+	> config.mak
+	command -v curl   2>/dev/null && echo "DL_CMD=curl -C - -L -o"  >> config.mak
+	command -v shasum 2>/dev/null && echo "SHA1_CMD=shasum -a 1 -c" >> config.mak
 	for i in x86_64-linux-musl aarch64-linux-musl i686-linux-musl; do
 		make -j TARGET=$i OUTPUT=~/.local install
 	done
