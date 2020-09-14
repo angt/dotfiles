73b262af3c3d589dbd20bc2ad75b275cec1f553a
Adrien Gallouët <adrien@gallouet.fr>
2020-09-14 20:15:15 +0000

    Bump go
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/go.sh
+++ .shell.d/go.sh
@@ -1,7 +1,3 @@
-VERSION=1.14.4
-OS=$(uname | tr '[:upper:]' '[:lower:]')
-TGZ=go$VERSION.$OS-amd64.tar.gz
-
 export GOROOT="$HOME/.goroot"
 export GOPATH="$HOME/.go"
 
@@ -9,6 +5,11 @@ shell_add_path "$GOROOT/bin"
 shell_add_path "$GOPATH/bin"
 
 shell_install_go() {
+	VERSION=1.15.2
+	OS=$(uname | tr '[:upper:]' '[:lower:]')
+	ARCH=$(uname -m)
+	[ "$ARCH" = x86_64 ] && ARCH=amd64
+	TGZ=go$VERSION.$OS-$ARCH.tar.gz
 	rm -rf "$GOROOT"
 	mkdir -p "$GOROOT"
 	curl -sSf "https://dl.google.com/go/$TGZ" | tar -xzf - -C "$GOROOT" --strip-components=1
