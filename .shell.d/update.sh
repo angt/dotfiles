a0374e11612a8a9b25a316b78810068f3baeea47
Adrien Gallouët <adrien@gallouet.fr>
2020-09-16 18:50:47 +0200

    Add a generic install
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/update.sh
+++ .shell.d/update.sh
@@ -2,4 +2,6 @@ shell_update() {
 	curl -sSf https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > ~/.shell.d/git.bash
 	curl -sSf https://raw.githubusercontent.com/rupa/z/master/z.sh > ~/.shell.d/z.bash
 	curl -sSfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	curl -sSf https://raw.githubusercontent.com/richfelker/install.sh/master/install.sh > ~/.bin/install
+	chmod +x ~/.bin/install
 }
