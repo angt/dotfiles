4f6208521602c4b03e831905d1c800f6aa639557
Adrien Gallouët <adrien@gallouet.fr>
2020-09-18 14:16:21 +0200

    Remove shell_install_totp
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/totp.sh
+++
@@ -1,8 +0,0 @@
-shell_install_totp() {
-	mkdir -p ~/.tmp
-	cd ~/.tmp || return
-	rm -rf totp
-	git clone https://github.com/angt/totp --recursive
-	cd totp || return
-	make prefix=~/.local install
-}
