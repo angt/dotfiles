19b5fb688a3aa38ad4f8268fe6100f518b52d223
Adrien Gallouët <adrien@gallouet.fr>
2020-09-18 14:13:43 +0200

    Use secret with totp
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/secret.sh
+++ .shell.d/secret.sh
@@ -1,8 +1,8 @@
-shell_install_secret() {
+shell_install_secret() (
+	set -e
 	mkdir -p ~/.tmp
-	cd ~/.tmp || return
+	cd ~/.tmp
 	rm -rf secret
-	git clone https://github.com/angt/secret --recursive
-	cd secret || return
-	make prefix=~/.local install
-}
+	git clone https://github.com/angt/secret -b totp --recursive
+	make -C secret CFLAGS="-Wall -O2 -march=native" DESTDIR=~/.local prefix=/usr install
+)
