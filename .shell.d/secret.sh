30395be4a2e0cac4b183f016c97df098c6fa862e
Adrien Gallouët <adrien@gallouet.fr>
2020-09-21 05:31:19 +0000

    Build a static secret
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/secret.sh
+++ .shell.d/secret.sh
@@ -4,5 +4,5 @@ shell_install_secret() (
 	cd ~/.tmp
 	rm -rf secret
 	git clone https://github.com/angt/secret -b totp --recursive
-	make -C secret CFLAGS="-Wall -O2 -march=native" DESTDIR=~/.local prefix=/usr install
+	make -C secret CFLAGS="-Wall -O2 -march=native -static" DESTDIR=~/.local prefix=/usr install
 )
