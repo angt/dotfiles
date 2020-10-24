8cb2587a03a61b90b38845ac2295fbdcd92ffcd6
Adrien Gallouët <adrien@gallouet.fr>
2020-10-24 11:55:21 +0200

    Install musl in ~/.local/usr
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/musl.sh
+++ .shell.d/musl.sh
@@ -9,6 +9,6 @@ shell_install_musl() (
 	command -v curl   2>/dev/null && echo "DL_CMD=curl -C - -L -o"  >> config.mak
 	command -v shasum 2>/dev/null && echo "SHA1_CMD=shasum -a 1 -c" >> config.mak
 	for i in x86_64-linux-musl aarch64-linux-musl i686-linux-musl; do
-		make -j TARGET=$i OUTPUT=~/.local install
+		make -j TARGET=$i OUTPUT=~/.local/usr install
 	done
 )
