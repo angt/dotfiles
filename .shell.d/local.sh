03e853e38a9885aeba176149915da9964e9edd82
Adrien Gallouët <adrien@gallouet.fr>
2020-10-24 11:55:52 +0200

    Update MANPATH
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/local.sh
+++ .shell.d/local.sh
@@ -2,3 +2,6 @@ shell_add_path ~/.local/bin
 shell_add_path ~/.local/sbin
 shell_add_path ~/.local/usr/bin
 shell_add_path ~/.local/usr/sbin
+
+shell_add_manpath ~/.local/usr/man
+shell_add_manpath ~/.local/usr/share/man
