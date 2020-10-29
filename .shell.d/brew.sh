195f86911248fca70655fbe047d99f7c25bd8d9c
Adrien Gallouët <adrien@gallouet.fr>
2020-10-29 12:35:06 +0000

    Fix shell_install_brew()
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/brew.sh
+++ .shell.d/brew.sh
@@ -7,9 +7,9 @@ shell_add_path "$HOMEBREW_PREFIX/bin"
 shell_add_path "$HOMEBREW_PREFIX/sbin"
 shell_add_manpath "$HOMEBREW_PREFIX/share/man"
 
-shell_install_brew() {
+shell_install_brew() (
 	set -e
 	rm -rf ~/.brew
 	git clone https://github.com/Homebrew/brew ~/.brew
 	brew analytics off
-}
+)
