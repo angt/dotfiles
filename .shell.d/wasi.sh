b7115cde53a9424350a161a857a7115365a1511a
Adrien Gallouët <adrien@gallouet.fr>
2021-01-21 20:02:07 +0000

    Add WASI
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

---
+++ .shell.d/wasi.sh
@@ -0,0 +1,14 @@
+shell_install_wasi() (
+	set -e
+	VERSION=12.0
+	DIR="$HOME/wasi-sdk-$VERSION"
+	OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+	[ "$OS"  = darwin ] && OS=macos
+	rm -rf "$DIR"
+	curl https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${VERSION%.*}/wasi-sdk-$VERSION-$OS.tar.gz | tar zxf - -C ~
+	cat > ~/.local/bin/wasicc <<-EOF
+	#!/bin/sh
+	exec $DIR/bin/clang --sysroot=$DIR/share/wasi-sysroot "\$@"
+	EOF
+	chmod +x ~/.local/bin/wasicc
+)
