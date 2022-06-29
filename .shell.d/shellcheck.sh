9d672fe83f14cbcbda9bb82d14e365669a91661c
Adrien Gallouët <adrien@gallouet.fr>
2022-06-29 18:21:17 +0200

    Fix shell_install_shellcheck() on mac
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/shellcheck.sh
+++ .shell.d/shellcheck.sh
@@ -4,11 +4,11 @@ shell_install_shellcheck() (
 	cd ~/.tmp
 	V="v0.8.0"
 	OS="$(uname | tr '[:upper:]' '[:lower:]')"
+	URL="https://github.com/koalaman/shellcheck/releases/download/$V/shellcheck-$V.$OS.x86_64.tar.xz"
 	case "$(uname -m)" in
 	(x86_64) ;;
 	(*) echo "Arch not supported"; exit 1;;
 	esac
-	curl -sSf "https://github.com/koalaman/shellcheck/releases/download/$V/shellcheck-$V.$OS.x86_64.tar.xz" \
-		| xz -d | tar -xf -
+	curl -sSf "$URL" | tar -xJf -
 	mv -f "shellcheck-$V/shellcheck" "$HOME/.local/bin"
 )
