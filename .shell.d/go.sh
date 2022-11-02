6c026ec645e9f9ea2082a8f2fdb686c5bfd80ea8
Adrien Gallouët <adrien@gallouet.fr>
2022-11-02 09:10:31 +0100

    Bump go
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/go.sh
+++ .shell.d/go.sh
@@ -7,7 +7,7 @@ shell_add_path "$GOPATH/bin"
 
 shell_install_go() (
 	set -e
-	VERSION=1.17.6
+	VERSION=1.19.3
 	OS=$(uname | tr '[:upper:]' '[:lower:]')
 	ARCH=$(uname -m)
 	[ "$ARCH" = x86_64 ] && ARCH=amd64
