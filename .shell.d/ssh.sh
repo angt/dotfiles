04817949fb63b6602abf1de624809a0bec68d4b1
Adrien Gallouët <adrien@gallouet.fr>
2024-03-14 11:39:40 +0100

    Fix ssh-agent
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/ssh.sh
+++ .shell.d/ssh.sh
@@ -1,10 +1,14 @@
 find ~/.ssh -exec grep -sq 'PRIVATE KEY' {} \; -and -exec chmod 400 {} \;
 
-{ . ~/.ssh-agent
-	if ! kill -0 "$SSH_AGENT_PID"; then
-		ssh-agent >~/.ssh-agent
-		. ~/.ssh-agent
-	fi
-} >/dev/null 2>/dev/null
+shell_ssh_agent() {
+	. ~/.ssh-agent
+
+	test -S "$SSH_AUTH_SOCK" && kill -0 "$SSH_AGENT_PID" && return
+
+	ssh-agent >~/.ssh-agent
+	. ~/.ssh-agent
+}
+
+shell_ssh_agent >/dev/null 2>/dev/null
 
 [ -f ~/.ssh/authorized_keys ] || cp ~/.ssh/id_ed25519.pub  ~/.ssh/authorized_keys
