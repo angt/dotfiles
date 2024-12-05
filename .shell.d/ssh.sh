00109674d9d5063a626d36dc4a1763ad716a93be
Adrien Gallouët <adrien@gallouet.fr>
2024-12-05 12:50:09 +0100

    Start .ssh-agent only if it's required
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/ssh.sh
+++ .shell.d/ssh.sh
@@ -1,10 +1,9 @@
 find ~/.ssh -exec grep -sq 'PRIVATE KEY' {} \; -and -exec chmod 400 {} \;
 
 shell_ssh_agent() {
+	test -S "$SSH_AUTH_SOCK" && return
 	. ~/.ssh-agent
-
 	test -S "$SSH_AUTH_SOCK" && kill -0 "$SSH_AGENT_PID" && return
-
 	ssh-agent >~/.ssh-agent
 	. ~/.ssh-agent
 }
