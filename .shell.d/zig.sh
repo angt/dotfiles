8cc26291e4238e69818094784eac0a349b5e5e18
Adrien Gallouët <adrien@gallouet.fr>
2024-12-08 22:37:19 +0100

    Support custom zig version
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/zig.sh
+++ .shell.d/zig.sh
@@ -1,6 +1,6 @@
 shell_install_zig() (
 	set -e
-	VERSION=0.13.0
+	VERSION=${1-"0.13.0"}
 	OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 	ARCH=$(uname -m)
 	[ "$OS"  = darwin ] && OS=macos
@@ -8,12 +8,20 @@ shell_install_zig() (
 	(amd64) ARCH=x86_64 ;;
 	(arm64) ARCH=aarch64 ;;
 	esac
-	NAME=zig-$OS-$ARCH-$VERSION
-	mkdir -p ~/.zig ~/.local/bin ~/.local/lib
-	rm -rf ~/.zig/$NAME
+	NAME="zig-$OS-$ARCH-$VERSION"
 	rm -rf ~/.local/bin/zig
 	rm -rf ~/.local/lib/zig
-	curl -sSf https://ziglang.org/download/$VERSION/$NAME.tar.xz | tar -Jxf- -C ~/.zig
-	ln -sf ~/.zig/$NAME/zig     ~/.local/bin/zig
-	ln -sf ~/.zig/$NAME/lib/zig ~/.local/lib/zig
+	if [ ! -e ~/.zig/"$NAME"/zig ]; then
+		mkdir -p ~/.zig ~/.local/bin ~/.local/lib
+		case "$VERSION" in
+		(*-dev.*) DIR="builds" ;;
+		(*)       DIR="download/$VERSION" ;;
+		esac
+		curl -sS \
+			-f "https://ziglang.org/$DIR/$NAME.tar.xz" \
+			-f "https://pkg.machengine.org/zig/$NAME.tar.xz" |
+			tar -Jxf- -C ~/.zig
+	fi
+	ln -sf ~/.zig/"$NAME"/zig     ~/.local/bin/zig
+	ln -sf ~/.zig/"$NAME"/lib/zig ~/.local/lib/zig
 )
