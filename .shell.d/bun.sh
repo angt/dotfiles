28e6f45b9d71e4b00d45bfbf37b2c9be78317334
Adrien Gallouët <adrien@gallouet.fr>
2025-01-14 12:34:52 +0100

    Add shell_install_bun
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

---
+++ .shell.d/bun.sh
@@ -0,0 +1,22 @@
+
+shell_install_bun() (
+	set -e
+	mkdir -p ~/.tmp
+	cd ~/.tmp
+	OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+	ARCH=$(uname -m)
+	case "$ARCH" in
+	(x86_64)
+		ARCH=x64
+		case "$OS" in
+		(darwin) sysctl machdep.cpu | grep -sq AVX2 ;;
+		(linux)  grep -sq avx2 /proc/cpuinfo ;;
+		esac && ARCH="$ARCH-baseline" ;;
+	(arm64) ARCH=aarch64 ;;
+	esac
+	NAME="bun-$OS-$ARCH"
+	URL="https://github.com/oven-sh/bun/releases/latest/download"
+	curl -OsSfL "$URL/$NAME.zip"
+	unzip -oq "$NAME.zip"
+	mv "$NAME/bun" ~/.local/bin/bun
+)
