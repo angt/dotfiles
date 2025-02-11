a229859e561ef68f217a0f1c19b5862a860a7ebd
Adrien Gallouët <adrien@gallouet.fr>
2025-02-11 15:42:08 +0000

    Remove ugly code
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/uv.sh
+++ .shell.d/uv.sh
@@ -1,14 +1,6 @@
 shell_setup_uv() {
 	shell_add_path ~/.venv/bin
-	mkdir -p ~/.tmp
-	cat > ~/.tmp/lib.py <<-'EOF'
-	import sys
-	for path in sys.path:
-	    if '/lib/' in path and 'site-packages' not in path:
-	        print(path.split('/lib/')[0] + '/lib')
-	        break
-	EOF
-	shell_add_lib_path $(python ~/.tmp/lib.py)
+	shell_add_lib_path $(python -c 'import sys; print(sys.base_exec_prefix + "/lib")')
 }
 
 shell_install_uv() {
