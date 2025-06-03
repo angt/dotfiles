79e97b8407e1bf6214499510e78bde3eda2bc186
Adrien Gallouët <angt@huggingface.co>
2025-06-03 10:55:14 +0200

    Use python 3.12 by default
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- .shell.d/uv.sh
+++ .shell.d/uv.sh
@@ -1,11 +1,12 @@
 shell_setup_uv() {
 	shell_add_path ~/.venv/bin
-	shell_add_lib_path $(python -c 'import sys; print(sys.base_exec_prefix + "/lib")')
+	shell_add_lib_path "$(python -c 'import sys; print(sys.base_exec_prefix + "/lib")')"
 }
 
 shell_install_uv() {
+	[ "$1" ] || set -- "3.12"
 	curl -LsSf https://astral.sh/uv/install.sh | sh &&
-	UV_PYTHON_PREFERENCE=only-managed uv venv "$@" ~/.venv &&
+	UV_PYTHON_PREFERENCE=only-managed uv venv -p "$1" ~/.venv &&
 	shell_setup_uv
 }
 
