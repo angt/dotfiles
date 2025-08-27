d8c820d4a602652d38dd212b39accda26315f0b5
Adrien Gallouët <angt@huggingface.co>
2025-08-27 14:21:03 +0200

    Update shell_install_llvm()
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- .shell.d/llvm.sh
+++ .shell.d/llvm.sh
@@ -7,10 +7,17 @@ shell_install_llvm() (
 	cd llvm-project
 	mkdir -p build
 	cd build
+	OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+	case "$OS" in
+	(darwin) LLVM_ENABLE_LLD=OFF ;;
+	(*)      LLVM_ENABLE_LLD=ON  ;;
+	esac
 	cmake -G Ninja ../llvm \
 		-DCMAKE_BUILD_TYPE=Release \
+		-DLLVM_ENABLE_LLD="$LLVM_ENABLE_LLD" \
 		-DLLVM_ENABLE_PROJECTS="clang;mlir" \
 		-DLLVM_TARGETS_TO_BUILD="AArch64;X86" \
+		-DMLIR_ENABLE_BINDINGS_PYTHON=ON \
 		-DCMAKE_INSTALL_PREFIX="$HOME/.local/usr"
 	ninja install
 )
