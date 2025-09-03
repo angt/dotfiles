32d464f1f44a752e3e779694ebb5ad66b101615b
Adrien Gallouët <adrien@gallouet.fr>
2025-09-03 15:41:43 +0000

    Remove LLD
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/llvm.sh
+++ .shell.d/llvm.sh
@@ -8,14 +8,8 @@ shell_install_llvm() (
 	[ "$1" ] && git checkout "$1"
 	mkdir -p build
 	cd build
-	OS=$(uname -s | tr '[:upper:]' '[:lower:]')
-	case "$OS" in
-	(darwin) LLVM_ENABLE_LLD=OFF ;;
-	(*)      LLVM_ENABLE_LLD=ON  ;;
-	esac
 	cmake -G Ninja ../llvm \
 		-DCMAKE_BUILD_TYPE=Release \
-		-DLLVM_ENABLE_LLD="$LLVM_ENABLE_LLD" \
 		-DLLVM_ENABLE_PROJECTS="clang;mlir" \
 		-DLLVM_TARGETS_TO_BUILD="AArch64;X86" \
 		-DCMAKE_INSTALL_PREFIX="$HOME/.local/usr"
