d296999ee540fcc2848e290236df0abe905bbceb
Adrien Gallouët <adrien@gallouet.fr>
2025-09-09 10:07:55 +0000

    Update gcc
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/musl.sh
+++ .shell.d/musl.sh
@@ -3,17 +3,14 @@ shell_install_musl() (
 	mkdir -p ~/.tmp
 	cd ~/.tmp
 	rm -rf musl-cross-make
-	git clone https://github.com/richfelker/musl-cross-make
+	git clone gh:angt/musl-cross-make
 	cd musl-cross-make
 	{	command -v curl   >&2 && echo "DL_CMD=curl -C - -L -o"
 		command -v shasum >&2 && echo "SHA1_CMD=shasum -a 1 -c"
-		echo "GCC_VER =  11.5.0"
+		echo "GCC_VER = 15.1.0"
 	} 2>/dev/null >config.mak
-	for i in {x86_64,i686}-linux-musl     \
-		     aarch64{,_be}-linux-musl     \
-		     arm{,eb}-linux-musleabi{,hf} \
-		     riscv64-linux-musl           \
-		     mips64{,el}-linux-musl{,n32}{,sf}
-		do make -j TARGET=$i OUTPUT=~/.local/usr install
+	for i in x86_64-linux-musl aarch64-linux-musl
+	do
+		make -j$(nproc) TARGET=$i OUTPUT=~/.local/usr install
 	done
 )
