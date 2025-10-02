df2156fdc864a18b5d029519ff935b96c918c589
Adrien Gallouët <angt@huggingface.co>
2025-10-02 17:58:41 +0200

    Update shellcheck
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- .shell.d/shellcheck.sh
+++ .shell.d/shellcheck.sh
@@ -1,15 +1,15 @@
 shell_install_shellcheck() (
+	[ "$1" ] || set -- "0.11.0"
 	set -e
 	mkdir -p ~/.tmp
 	cd ~/.tmp
-	V="v0.10.0"
 	OS="$(uname | tr '[:upper:]' '[:lower:]')"
 	ARCH="$(uname -m)"
 	case "$ARCH" in
 	(amd64) ARCH=x86_64 ;;
 	(arm64) ARCH=aarch64 ;;
 	esac
-	URL="https://github.com/koalaman/shellcheck/releases/download/$V/shellcheck-$V.$OS.$ARCH.tar.xz"
+	URL="https://github.com/koalaman/shellcheck/releases/download/v$1/shellcheck-v$1.$OS.$ARCH.tar.xz"
 	curl -sSf "$URL" | tar -xJf -
-	mv -f "shellcheck-$V/shellcheck" "$HOME/.local/bin"
+	mv -f "shellcheck-v$1/shellcheck" "$HOME/.local/bin"
 )
