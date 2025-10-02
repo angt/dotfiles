0edbb71e0d3cf9d8c919cb05df022fefad588125
Adrien Gallouët <angt@huggingface.co>
2025-10-02 17:40:46 +0200

    Cleanup
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- .shell.d/brew.sh
+++ .shell.d/brew.sh
@@ -3,7 +3,6 @@ shell_setup_brew() {
 	export HOMEBREW_CELLAR="$HOMEBREW_PREFIX/Cellar"
 	export HOMEBREW_REPOSITORY="$HOMEBREW_PREFIX"
 	export HOMEBREW_NO_ANALYTICS=1
-
 	shell_add_path "$HOMEBREW_PREFIX/bin"
 	shell_add_path "$HOMEBREW_PREFIX/sbin"
 	shell_add_manpath "$HOMEBREW_PREFIX/share/man"
