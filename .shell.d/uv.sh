57f0b4fa508812682a6d9363838424235516e012
Adrien Gallouët <angt@huggingface.co>
2025-10-02 16:43:16 +0200

    Update uv
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- .shell.d/uv.sh
+++ .shell.d/uv.sh
@@ -1,13 +1,13 @@
 shell_setup_uv() {
 	shell_add_path ~/.venv/bin
-	shell_add_lib_path "$(python -c 'import sys; print(sys.base_exec_prefix + "/lib")')"
+	shell_add_lib_path "$(~/.venv/bin/python -c 'import sys; print(sys.base_exec_prefix + "/lib")')"
 }
 
 shell_install_uv() {
 	[ "$1" ] || set -- "3.12"
 	curl -LsSf https://astral.sh/uv/install.sh | sh &&
-	UV_PYTHON_PREFERENCE=only-managed uv venv -p "$1" ~/.venv &&
+	UV_PYTHON_PREFERENCE=only-managed uv venv -p "$1" --clear ~/.venv &&
 	shell_setup_uv
 }
 
-[ -e ~/.venv ] && shell_setup_uv
+[ -x ~/.venv/bin/python ] && shell_setup_uv
