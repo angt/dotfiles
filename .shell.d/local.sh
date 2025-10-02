ff0fa860aa7bcd094fe429465b0172e43629d6d4
Adrien Gallouët <angt@huggingface.co>
2025-10-02 16:03:18 +0200

    Simplify ~/.local
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- .shell.d/local.sh
+++ .shell.d/local.sh
@@ -1,11 +1,20 @@
+mkdir -p \
+	~/.local/usr/bin \
+	~/.local/usr/share/man \
+	~/.local/usr/lib/pkgconfig \
+	~/.local/usr/include
+
+ln -sfn bin         ~/.local/usr/sbin
+ln -sfn share/man   ~/.local/usr/man
+ln -sfn usr/bin     ~/.local/bin
+ln -sfn usr/sbin    ~/.local/sbin
+ln -sfn usr/man     ~/.local/man
+ln -sfn usr/share   ~/.local/share
+ln -sfn usr/lib     ~/.local/lib
+ln -sfn usr/include ~/.local/include
+
 shell_add_path ~/.local/bin
-shell_add_path ~/.local/sbin
-shell_add_path ~/.local/usr/bin
-shell_add_path ~/.local/usr/sbin
-
-shell_add_manpath ~/.local/usr/man
-shell_add_manpath ~/.local/usr/share/man
-
+shell_add_manpath ~/.local/man
 shell_add_lib_path ~/.local/lib
 
 shell_setup_local_dev() {
