6d80ff3fa2284f48d7d152c90ca5010994288a7f
Adrien Gallouët <angt@huggingface.co>
2025-10-02 16:57:40 +0200

    Update cmake
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- .shell.d/cmake.sh
+++ .shell.d/cmake.sh
@@ -1,11 +1,10 @@
 shell_install_cmake() (
 	set -e
-	[ "$1" ] || set -- "4.1.1"
+	[ "$1" ] || set -- "4.1.2"
+	rm -rf ~/.tmp/cmake
 	mkdir -p ~/.tmp
-	cd ~/.tmp
-	rm -rf CMake
-	git clone --branch "v$1" https://github.com/Kitware/CMake
-	cd CMake
+	git clone --branch "v$1" https://github.com/Kitware/CMake ~/.tmp/cmake
+	cd ~/.tmp/cmake
 	./bootstrap --prefix="$HOME/.local" --parallel=$(nproc)
 	make -j$(nproc) install
 )
