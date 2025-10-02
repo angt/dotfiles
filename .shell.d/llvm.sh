8377414246e92b67a1dff741f73e24a22ae2ca73
Adrien Gallouët <angt@huggingface.co>
2025-10-02 17:08:32 +0200

    Update llvm
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- .shell.d/llvm.sh
+++ .shell.d/llvm.sh
@@ -1,17 +1,14 @@
 shell_install_llvm() (
 	set -e
+	rm -rf ~/.tmp/llvm
 	mkdir -p ~/.tmp
-	cd ~/.tmp
-	rm -rf llvm-project
-	git clone https://github.com/llvm/llvm-project
-	cd llvm-project
+	git clone https://github.com/llvm/llvm-project ~/.tmp/llvm
+	cd ~/.tmp/llvm
 	[ "$1" ] && git checkout "$1"
-	mkdir -p build
-	cd build
-	cmake -G Ninja ../llvm \
+	cmake -S llvm -B build -G Ninja \
 		-DCMAKE_BUILD_TYPE=Release \
 		-DLLVM_ENABLE_PROJECTS="clang;mlir" \
 		-DLLVM_TARGETS_TO_BUILD="AArch64;X86" \
 		-DCMAKE_INSTALL_PREFIX="$HOME/.local/usr"
-	ninja install
+	cmake --build build --target install
 )
