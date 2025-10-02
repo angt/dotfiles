2fd71b486b6f1722246ef3ae90b0a29fdca26ba5
Adrien Gallouët <angt@huggingface.co>
2025-10-02 16:08:24 +0200

    Update ninja
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- .shell.d/ninja.sh
+++ .shell.d/ninja.sh
@@ -1,11 +1,10 @@
 shell_install_ninja() (
 	set -e
 	[ "$1" ] || set -- "v1.12.1"
+	rm -rf ~/.tmp/ninja
 	mkdir -p ~/.tmp
-	cd ~/.tmp
-	rm -rf ninja
-	git clone --branch "$1" https://github.com/ninja-build/ninja
-	cd ninja
+	git clone --branch "$1" https://github.com/ninja-build/ninja ~/.tmp/ninja
+	cd ~/.tmp/ninja
 	./configure.py --bootstrap
 	mv ninja ~/.local/bin
 )
