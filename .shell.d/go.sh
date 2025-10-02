03abf65d837e83fd9918e298f5078f3769fa9720
Adrien Gallouët <angt@huggingface.co>
2025-10-02 17:40:02 +0200

    Update go
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- .shell.d/go.sh
+++ .shell.d/go.sh
@@ -2,28 +2,28 @@ shell_setup_go() {
 	export GOROOT="$HOME/.goroot/go"
 	export GOPATH="$HOME/.go"
 	export GO111MODULE="on"
-
 	shell_add_path "$GOROOT/bin"
 	shell_add_path "$GOPATH/bin"
 }
 
+__shell_install_go() (
+	[ "$1" ] || set -- "1.25.1"
+	set -e
+	OS=$(uname | tr '[:upper:]' '[:lower:]')
+	ARCH=$(uname -m)
+	case "$ARCH" in
+	(x86_64)  ARCH=amd64 ;;
+	(aarch64) ARCH=arm64 ;;
+	esac
+	TGZ=go$1.$OS-$ARCH.tar.gz
+	rm -rf ~/.goroot
+	mkdir -p ~/.goroot
+	curl -sSf "https://dl.google.com/go/$TGZ" |
+		tar -xzf - -C ~/.goroot
+)
+
 shell_install_go() {
-	[ "$1" ] || set -- "1.24.3"
-	(
-		set -e
-		OS=$(uname | tr '[:upper:]' '[:lower:]')
-		ARCH=$(uname -m)
-		case "$ARCH" in
-		(x86_64)  ARCH=amd64 ;;
-		(aarch64) ARCH=arm64 ;;
-		esac
-		TGZ=go$1.$OS-$ARCH.tar.gz
-		rm -rf ~/.goroot
-		mkdir -p ~/.goroot
-		curl -sSf "https://dl.google.com/go/$TGZ" |
-			tar -xzf - -C ~/.goroot
-	) &&
-	shell_setup_go
+	__shell_install_go && shell_setup_go
 }
 
 [ -e ~/.goroot/go ] && shell_setup_go
