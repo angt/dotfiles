16d9c5737a169d4aead233b0414c3f020c22f9db
Adrien Gallouët <angt@huggingface.co>
2025-10-02 16:03:51 +0200

    Update vim
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- .shell.d/vim.sh
+++ .shell.d/vim.sh
@@ -1,29 +1,18 @@
-shell_setup_vim() {
-	shell_add_path ~/.vimroot/bin
-	shell_add_manpath ~/.vimroot/share/man
-}
+shell_install_vim() (
+	set -e
+	rm -rf ~/.tmp/vim
+	mkdir -p ~/.tmp
+	git clone https://github.com/vim/vim --depth 1 ~/.tmp/vim
+	cd ~/.tmp/vim
+	./configure \
+		--prefix="$HOME/.local/usr" \
+		--disable-nls \
+		--disable-gui
+	make install -j"$(nproc)"
+)
 
-shell_install_vim() {
-	(
-		set -e
-		rm -rf ~/.vimroot ~/.tmp/vim
-		mkdir -p ~/.tmp
-		git clone https://github.com/vim/vim --depth 1 ~/.tmp/vim
-		cd ~/.tmp/vim
-		./configure \
-			--prefix="$HOME/.vimroot" \
-			--disable-nls \
-			--disable-gui
-		make -j install
-	) &&
-	shell_setup_vim
-}
-
-[ -e ~/.vimroot ] && shell_setup_vim
-
-command -v vim 1>/dev/null || return
-
-export VISUAL=vim
-export EDITOR=vim
-
-alias vi=vim
+if command -v vim 1>/dev/null; then
+	export VISUAL=vim
+	export EDITOR=vim
+	alias vi=vim
+fi
