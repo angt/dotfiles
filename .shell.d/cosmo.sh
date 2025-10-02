9ccf06acd0a31e014e11cbf2637fb2c0b2e8dcae
Adrien Gallouët <angt@huggingface.co>
2025-10-02 17:41:28 +0200

    Update cosmo
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- .shell.d/cosmo.sh
+++ .shell.d/cosmo.sh
@@ -2,13 +2,16 @@ shell_setup_cosmo() {
 	shell_add_path ~/.cosmo/bin
 }
 
-shell_install_cosmo() (
+__shell_install_cosmo() (
 	rm -rf ~/.cosmo
 	mkdir -p ~/.cosmo
 	cd ~/.cosmo || return
 	curl -O https://cosmo.zip/pub/cosmocc/cosmocc.zip
 	unzip cosmocc.zip
-	shell_setup_cosmo
 )
+
+shell_install_cosmo() {
+	__shell_install_cosmo && shell_setup_cosmo
+}
 
 [ -d ~/.cosmo/bin ] && shell_setup_cosmo
