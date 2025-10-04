87324867625df424b230e8f418fa152853f7a458
Adrien Gallouët <adrien@gallouet.fr>
2025-10-04 08:47:03 +0200

    Add git
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

---
+++ .shell.d/git.sh
@@ -0,0 +1,12 @@
+shell_install_git() (
+	[ "$1" ] || set -- "2.51.0"
+	set -e
+	rm -rf ~/.tmp/git-*
+	mkdir -p ~/.tmp
+	curl -L "https://www.kernel.org/pub/software/scm/git/git-$1.tar.gz" |
+		tar zxf - -C ~/.tmp
+	cd ~/.tmp/"git-$1"
+	make configure
+	./configure --prefix="$HOME/.local/usr"
+	make install -j"$(nproc)"
+)
