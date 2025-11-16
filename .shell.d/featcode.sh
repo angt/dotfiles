df0f365aaa70847a016c953f29143eeb727a02e7
Adrien Gallouët <angt@huggingface.co>
2025-11-16 23:11:20 +0100

    Update shell_install_featcode
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- .shell.d/featcode.sh
+++ .shell.d/featcode.sh
@@ -1,13 +1,4 @@
 shell_install_featcode() (
-	[ "$1" ] || set -- "v3"
-	URL="https://github.com/angt/featcode/releases/download"
-	OS=$(uname -s | tr '[:upper:]' '[:lower:]')
-	ARCH=$(uname -m)
-	[ "$OS" = darwin ] && OS=macos
-	case "$ARCH" in
-	(amd64) ARCH=x86_64 ;;
-	(arm64) ARCH=aarch64 ;;
-	esac
-	curl -sSf "$URL/$1/$ARCH-$OS-featcode" > ~/.local/bin/featcode
-	chmod +x ~/.local/bin/featcode
+	cd ~/.local/bin &&
+	curl cdn.statically.io/gh/angt/featcode/master/install.sh | sh
 )
