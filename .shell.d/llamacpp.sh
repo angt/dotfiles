991f3910909179deeb16092d327315f06bf9c907
Adrien Gallouët <angt@huggingface.co>
2025-11-20 19:18:37 +0100

    Use installama.sh
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- .shell.d/llamacpp.sh
+++ .shell.d/llamacpp.sh
@@ -1,18 +1,9 @@
 shell_install_llamacpp() (
-	set -e
-	rm -rf ~/.tmp/llama.cpp
-	mkdir -p ~/.tmp
-	git clone --depth 1 https://github.com/ggml-org/llama.cpp ~/.tmp/llama.cpp
-	cd ~/.tmp/llama.cpp
-	cmake -B build \
-		-DLLAMA_CURL=OFF \
-		-DLLAMA_OPENSSL=ON \
-		-DCMAKE_BUILD_TYPE=Release \
-		-DCMAKE_INSTALL_PREFIX="$HOME/.local"
-	cmake --build build -j"$(nproc)" --target install
+	curl angt.github.io/installama.sh | sh
 )
 
+# shellcheck disable=2088
 shell_setup_llamacpp() {
 	tmux new-session -d -s llamacpp &&
-	tmux send-keys -t llamacpp 'llama-server --fim-qwen-14b-spec' C-m
+	tmux send-keys -t llamacpp '~/.installama/llama-server --fim-qwen-14b-spec' C-m
 }
