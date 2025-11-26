543514a63f99004fab5068233f8f45f37985b20e
Adrien Gallouët <angt@huggingface.co>
2025-11-26 10:29:23 +0100

    Simplify zig
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- .shell.d/zig.sh
+++ .shell.d/zig.sh
@@ -1,6 +1,5 @@
 shell_setup_zig() {
-	[ "$1" ] && ln -sfn ~/.zig/zig-*-"$1" ~/.zig/current
-	shell_add_path ~/.zig/current
+	[ "$1" ] && ln -sf ~/.zig/zig-*-"$1"/zig ~/.local/bin/zig
 }
 
 __shell_install_zig() (
@@ -8,18 +7,17 @@ __shell_install_zig() (
 	ARCH=$(uname -m)
 	[ "$OS" = darwin ] && OS=macos
 	case "$ARCH" in
-		(amd64) ARCH=x86_64 ;;
-		(arm64) ARCH=aarch64 ;;
+	(amd64) ARCH=x86_64 ;;
+	(arm64) ARCH=aarch64 ;;
 	esac
-	mkdir -p ~/.zig
-	rm -rf ~/.zig/zig-*-"$1"
-	curl -sSf "https://ziglang.org/download/$1/zig-$ARCH-$OS-$1.tar.xz" |
-		tar -Jxf- -C ~/.zig
+	ZIG="zig-$ARCH-$OS-$1"
+	[ -d ~/.zig/"$ZIG" ] && return
+	mkdir -p ~/.zig &&
+	curl -sSf "https://ziglang.org/download/$1/$ZIG.tar.xz" |
+		tar -Jxf - -C ~/.zig
 )
 
 shell_install_zig() {
 	[ "$1" ] || set -- "0.15.2"
 	__shell_install_zig "$1" && shell_setup_zig "$1"
 }
-
-[ -e ~/.zig/current ] && shell_setup_zig
