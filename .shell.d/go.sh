dfc043642a8deaa5761426581273e4ef1ed7cd92
Adrien Gallouët <angt@huggingface.co>
2025-11-26 10:30:21 +0100

    Update go
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

--- .shell.d/go.sh
+++ .shell.d/go.sh
@@ -7,23 +7,22 @@ shell_setup_go() {
 }
 
 __shell_install_go() (
-	[ "$1" ] || set -- "1.25.1"
-	set -e
 	OS=$(uname | tr '[:upper:]' '[:lower:]')
 	ARCH=$(uname -m)
 	case "$ARCH" in
 	(x86_64)  ARCH=amd64 ;;
 	(aarch64) ARCH=arm64 ;;
 	esac
-	TGZ=go$1.$OS-$ARCH.tar.gz
+	GO="go$1.$OS-$ARCH"
 	rm -rf ~/.goroot
-	mkdir -p ~/.goroot
-	curl -sSf "https://dl.google.com/go/$TGZ" |
+	mkdir -p ~/.goroot &&
+	curl -sSf "https://dl.google.com/go/$GO.tar.gz" |
 		tar -xzf - -C ~/.goroot
 )
 
 shell_install_go() {
-	__shell_install_go && shell_setup_go
+	[ "$1" ] || set -- "1.25.4"
+	__shell_install_go "$1" && shell_setup_go
 }
 
 [ -e ~/.goroot/go ] && shell_setup_go
