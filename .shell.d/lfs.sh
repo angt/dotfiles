2e04c52040670e4dbc92b1c2bef71e5eae116ed5
Adrien Gallouët <angt@huggingface.co>
2025-12-16 16:33:28 +0100

    Add shell_install_lfs
    Signed-off-by: Adrien Gallouët <angt@huggingface.co>

---
+++ .shell.d/lfs.sh
@@ -0,0 +1,17 @@
+shell_install_lfs() (
+	[ "$1" ] || set -- "v3.7.1"
+	URL="https://github.com/git-lfs/git-lfs/releases/download"
+	OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+	ARCH=$(uname -m)
+	case "$ARCH" in
+	(x86_64)  ARCH=amd64 ;;
+	(aarch64) ARCH=arm64 ;;
+	esac
+	case "$OS" in
+	(darwin) EXT=zip Z= ;;
+	(*)      EXT=tar.gz Z=z;;
+	esac
+	curl -sSfL "$URL/$1/git-lfs-$OS-$ARCH-$1.$EXT" |
+		tar -xO$Z -f - "*/git-lfs" > ~/.local/bin/git-lfs
+	chmod +x ~/.local/bin/git-lfs
+)
