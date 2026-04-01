69b2991c05ea79e329b636bc1ec81292ca1da553
Adrien Gallouët <adrien@gallouet.fr>
2026-04-01 09:25:41 +0000

    Remove llamacpp.sh
    Signed-off-by: Adrien Gallouët <adrien@gallouet.fr>

--- .shell.d/llamacpp.sh
+++
@@ -1,9 +0,0 @@
-shell_install_llamacpp() (
-	curl angt.github.io/installama.sh | sh
-)
-
-# shellcheck disable=2088
-shell_setup_llamacpp() {
-	tmux new-session -d -s llamacpp &&
-	tmux send-keys -t llamacpp '~/.installama/llama-server --fim-qwen-14b-spec' C-m
-}
